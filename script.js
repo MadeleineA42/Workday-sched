b7fb0613d411a0719793210d36e8bf29dbad6671
Madeleine Abraham <abraham.madeleine229@gmail.com>
2023-08-22 17:51:46 -0400

    added function to change hour block colors

--- script.js
+++ script.js
@@ -5,17 +5,26 @@
 $(document).ready(function() {
   // this is a function to display the current date
  function displayCurrentDate()  {
-  const currentDate = day.js().format("MMMM D, YYYY");
+  const currentDate = day.js().format("MM,DD,YYYY");
   $("#currentDay").text(currentDate);
  }
- // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-});
+ 
+// this function allows the hour blocks to change color based on the current hour 
+ function hourBlockColor()  {
+  const currentHour = day.js().hour('h');
+  $('.time-block').each(function()  {
+    const blockHour = parseInt(this.id);
+    if (blockHour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
+  });
+ }
+
+
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -28,3 +37,12 @@ $(document).ready(function() {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+ 
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+});
