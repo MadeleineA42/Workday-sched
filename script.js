78f2b57ad3a17fe8658e0e487f0bed4f98ab729f
Madeleine Abraham <abraham.madeleine229@gmail.com>
2023-08-22 17:04:50 -0400

    added current date display day.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+$(document).ready(function() {
+  // this is a function to display the current date
+ function displayCurrentDate()  {
+  const currentDate = day.js().format("MMMM D, YYYY");
+  $("#currentDay").text(currentDate);
+ }
  // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +15,8 @@
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+});
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,6 +28,3 @@
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-$(document).ready(function() {
- 
-});
