0cac94bf39a94d73a5796fb98a1e3b6d5e257ad0
Madeleine Abraham <abraham.madeleine229@gmail.com>
2023-08-25 20:25:36 -0400

    edited js for readability

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
+// this will ensure that the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
 $(document).ready(function () {
-  // this is a variable to display the current date
-  // TODO: Add code to display the current date in the header of the page.
+  // this is a variable to display the current date and display the current date in the header of the page.
   let currentHour = moment().format('H');
   let currentDateAndTime = moment().format('ddd' + ' ' + 'MMM' + ' ' + 'Do' + ',' + 'YYYY');
   $("#currentDay").text(currentDateAndTime);
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+
   // this function allows the hour blocks to change color based on the current hour 
   function hourBlockColor() {
 
@@ -23,13 +18,8 @@ $(document).ready(function () {
       $(this).toggleClass('future', blockHour > currentHour)
     });
   }
-   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+ 
+  // event listener for clicks on the save buttons
   // this will allow you to save events to local storage 
   function saveEvent() {
     $('.saveBtn').on('click', function () {
@@ -38,10 +28,8 @@ $(document).ready(function () {
       localStorage.setItem(key, value);
     });
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // This code will get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. 
   function loadEvent() {
   $('.time-block').each(function () {
     const key = $(this).attr('id');
